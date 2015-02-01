695b25b9bce37b096a3a7fba054f4212072d7cdf
asadisaghar <asadisaghar@gmail.com>
2015-02-01 18:34:34 +0100

    draw the board

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Character(object):
         self.position = None
         self.place(position)
 
+# puts the character *self* in the given *position*
     def place(self, position):
         if self.position != None:
             self.position.who.remove(self)
@@ -15,7 +16,7 @@ class Character(object):
         if position != None:
             position.who.append(self)
 
-
+# moves the character *self* to its given *direction*
     def move(self, direction):
         if direction == "north":
             self.place(self.position.north)
@@ -55,6 +56,7 @@ class Room(object):
         self.east = None
         self.who = []
 
+# set two rooms *self*, and *other* as neighbors
     def neighbor(self, other, direction):
         if direction == "north":
             self.north = other
@@ -79,50 +81,64 @@ class Room(object):
             for monster_to_fight in monsters_in_room:
                 player.fight(monster_to_fight)
 
-
 class Game(object):
     def __init__(self, board_edge_length, monster_nr):
-        self.board = []
+        self.rooms = []
         for room in xrange(board_edge_length**2):
-            self.board.append(Room(room))
+            self.rooms.append(Room(room))
         def room_index(i, j):
             return j*board_edge_length+i
         for room_i in xrange(board_edge_length):
             for room_j in xrange(board_edge_length):
-                room = self.board[room_index(room_i, room_j)]
+                room = self.rooms[room_index(room_i, room_j)]
                 if room_i == 0:
-                    room.neighbor(self.board[room_index(board_edge_length-1, room_j)], "west")
+                    room.neighbor(self.rooms[room_index(board_edge_length-1, room_j)], "west")
                 if room_i == board_edge_length-1:
-                    room.neighbor(self.board[room_index(0, room_j)], "east")
+                    room.neighbor(self.rooms[room_index(0, room_j)], "east")
                 if room_j == 0:
-                    room.neighbor(self.board[room_index(room_i, board_edge_length-1)], "south")
+                    room.neighbor(self.rooms[room_index(room_i, board_edge_length-1)], "south")
                 if room_j == board_edge_length-1:
-                    room.neighbor(self.board[room_index(room_i, 0)], "north")
+                    room.neighbor(self.rooms[room_index(room_i, 0)], "north")
                 else: 
-                    room.neighbor(self.board[room_index(room_i, room_j+1)], "north")
-                    room.neighbor(self.board[room_index(room_i, room_j-1)], "south")
-                    room.neighbor(self.board[room_index(room_i-1, room_j)], "west")
-                    room.neighbor(self.board[room_index(room_i+1, room_j)], "east")
+                    room.neighbor(self.rooms[room_index(room_i, room_j+1)], "north")
+                    room.neighbor(self.rooms[room_index(room_i, room_j-1)], "south")
+                    room.neighbor(self.rooms[room_index(room_i-1, room_j)], "west")
+                    room.neighbor(self.rooms[room_index(room_i+1, room_j)], "east")
         player_position = np.random.random_integers(board_edge_length)
-        self.player = Player(self.board[player_position], FULL_LIFE)
+        self.player = Player(self.rooms[player_position], FULL_LIFE)
         self.monsters = []
         monster_positions = np.random.random_integers(board_edge_length**2, size=monster_nr)
         monster_strengths = np.random.random_integers(MAX_MONSTER_STRENGTH, size=monster_nr)
         for monster in xrange(monster_nr):
-            self.monsters.append(Monster(self.board[monster_positions[monster]], monster_strengths[monster]))
+            self.monsters.append(Monster(self.rooms[monster_positions[monster]], monster_strengths[monster]))
+
+# draw the board
+        i = 0
+        for room in self.rooms:
+            i += 1
+            if room == self.player.position:
+                print 'P\t'
+            j = 0
+            for j in range(monster_nr):
+                if room == self.monsters[j].position:
+                    print 'M%d\t'%(j)
+            else:
+                print 'X\t'
+            if i%board_edge_length==0:
+                print '\n'
+            
 
 g1 = Game(4, 2)
 #                if room_i == 0:
-#                    room.neighbor(self.board[room_i+board_edge_length, room_j], "west")
+#                    room.neighbor(self.rooms[room_i+board_edge_length, room_j], "west")
 #                if room_i == board_edge_length-1:
-#                    room.neighbor(self.board[0, room_j], "east")
+#                    room.neighbor(self.rooms[0, room_j], "east")
 #                if room_j == 0:
-#                    room.neighbor(self.board[room_i, room_j+board_edge_length], "south")
+#                    room.neighbor(self.rooms[room_i, room_j+board_edge_length], "south")
 #                if room_j == board_edge_length-1:
-#                    room.neighbor(self.board[room_i, 0], "north")
+#                    room.neighbor(self.rooms[room_i, 0], "north")
 #                else: 
-#                    room.neighbor(self.board[room_index(room_i, room_j+1)], "north")
-#                    room.neighbor(self.board[room_i, room_j-1], "south")
-#                    room.neighbor(self.board[room_i-1, room_j], "west")
-#                    room.neighbor(self.board[room_i+1, room_j], "east")
-
+#                    room.neighbor(self.rooms[room_index(room_i, room_j+1)], "north")
+#                    room.neighbor(self.rooms[room_i, room_j-1], "south")
+#                    room.neighbor(self.rooms[room_i-1, room_j], "west")
+#                    room.neighbor(self.rooms[room_i+1, room_j], "east")
